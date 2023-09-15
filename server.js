970dcf28a84848d71a44b8828ec2002809a5d43b
reflection4046 <nahidulaiub@gmail.com>
2023-09-15 20:47:39 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const colors = require('colors')
+const dotenv =require('dotenv')
+
+
+//env config 
+dotenv.config()
+
+//rest object
+const app = express()
+
+//middelwares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+//routes
+app.get('/', (req,res)=>{
+    res.status(200).send ({
+        "message": "Node Server"
+    })
+})
+//port
+const PORT = process.env.PORT || 8080
+
+
+//listen
+app.listen(8080, ()=>{
+    console.log(`server Running on ${ process.env.DEV_MODE} port no ${PORT}`.bgCyan.white)
+})
+
